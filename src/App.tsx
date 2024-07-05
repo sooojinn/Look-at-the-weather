a6357b5e17f7d8576b765dabf69b3feab6d0322a
seungchanwoo <tmdcksqkd123@naver.com>
2024-07-05 12:58:31 +0900

    Style: 기본 페이지 레이아웃 작성
    - 홈 화면 레이아웃 및 로그인 레이아웃 작성
    - 전체 페이지 기본 크기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Outlet />
+    <div className="bg-gray-100">
+      <div className="max-w-md m-auto min-h-screen bg-white">
+        <Outlet />
+      </div>
     </div>
   );
 }
