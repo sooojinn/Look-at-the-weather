760494236a101e735733aefa6d3a57e39a7126dd
seungchanwoo <tmdcksqkd123@naver.com>
2024-07-09 15:29:48 +0900

    feat: 로그인 기능 추가
    - msw 설정 추가
    - authStore zusthand 전역관리 추가
    - 로그인 기능 구현
    - accessToken 로컬스토리지 저장
    - refreshToken 쿠키 저장

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,10 +1,22 @@
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 import PageTitle from '../components/common/PageTitle';
+import { BASEURL } from '../constants/constants';
+import { useNavigate } from 'react-router-dom';
 
 export default function Signup() {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    watch,
+    reset,
+    // formState: { errors },
+  } = useForm();
   const [showTerms, setShowTerms] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
-
+  const navigate = useNavigate();
   const toggleTerms = () => {
     setShowTerms(!showTerms);
   };
@@ -12,22 +24,56 @@ export default function Signup() {
   const handleCheckboxChange = () => {
     setIsChecked(!isChecked);
   };
-
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    if (isChecked) {
-      // 이 부분에 가입 처리 로직을 추가할 수 있습니다.
-      alert('가입 처리를 진행합니다.');
-    } else {
-      alert('약관에 동의해야 가입할 수 있습니다.');
+  //이메일 발송
+  const handleSendVerification = async () => {
+    try {
+      const response = await axios.post(`${BASEURL}/api/v1/email/send-verification`, {});
+      // API 호출 후 처리 로직 추가
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      console.log(BASEURL);
     }
+  };
+  //이메일 인증코드 확인
+  const handleVerifyCode = async () => {
+    try {
+      const response = await axios.post(`${BASEURL}/api/v1/email/verify-code`, {});
+      // API 호출 후 처리 로직 추가
+      console.log(response);
+    } catch (error) {}
+  };
+  //닉네임 중복확인
+  const handleCheckNickname = async () => {
+    try {
+      const response = await axios.post(`${BASEURL}/api/v1/users/check-nickname`, {});
+      // API 호출 후 처리 로직 추가
+      console.log(response);
+    } catch (error) {}
+  };
+  // 회원가입
+  const onSubmit = async (data: any) => {
+    try {
+      const response = await axios.post(`${BASEURL}/api/v1/users/register`, data);
+      console.log(response);
+      alert('가입 처리를 진행합니다.');
+      reset();
+      navigate('/');
+    } catch (error) {
+      setError('submit', { message: '가입 처리 중 오류가 발생했습니다.' });
+    }
+  };
+
+  //validation
+  const validatePassword = (value: string) => {
+    return value === watch('password') || '비밀번호가 일치하지 않습니다';
   };
 
   return (
     <div>
       <PageTitle title="회원가입" />
-      <form className="px-5" onSubmit={handleSubmit}>
-        {/* 로그인 */}
+      <form className="px-5" onSubmit={handleSubmit(onSubmit)}>
+        {/* 이메일 */}
         <div>
           <label className="block mb-2 my-5 text-gray-600 font-bold">
             이메일<span className="text-red-500">*</span>
@@ -37,16 +83,28 @@ export default function Signup() {
               type="email"
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
               placeholder="(예시) abcde@naver.com"
+              {...register('email', {
+                required: true,
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message: '이메일 형식에 맞춰 작성해주세요',
+                },
+                maxLength: {
+                  value: 30,
+                  message: '30글자 미만 작성해주세요',
+                },
+              })}
             />
             <button
               type="button"
               className="rounded-lg bg-interactive-light hover:bg-primary-lightest py-3 px-4 whitespace-nowrap"
+              onClick={handleSendVerification}
             >
               인증번호 전송
             </button>
           </div>
         </div>
-        {/* 인증번호확인 */}
+        {/* 인증번호 확인 */}
         <div>
           <label className="block mb-2 my-4 text-gray-600 font-bold">
             인증번호 확인<span className="text-red-500">*</span>
@@ -56,10 +114,12 @@ export default function Signup() {
               type="text"
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
               placeholder="인증번호를 입력해 주세요."
+              {...register('code', { required: true })}
             />
             <button
               type="button"
               className="rounded-lg bg-interactive-light hover:bg-primary-lightest py-3 px-4 whitespace-nowrap"
+              onClick={handleVerifyCode}
             >
               인증번호 확인
             </button>
@@ -74,6 +134,17 @@ export default function Signup() {
             type="password"
             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
             placeholder="영문/숫자/특수문자 2가지 이상 조합 (8~15자)"
+            {...register('password', {
+              required: true,
+              minLength: {
+                value: 8,
+                message: '8글자 이상 작성해주세요.',
+              },
+              maxLength: {
+                value: 16,
+                message: '15글자 이하 작성해주세요.',
+              },
+            })}
           />
         </div>
         {/* 비밀번호 확인 */}
@@ -85,6 +156,7 @@ export default function Signup() {
             type="password"
             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
             placeholder="비밀번호를 한번 더 입력해 주세요."
+            {...register('confirmPassword', { required: true, validate: validatePassword })}
           />
         </div>
         {/* 이름 */}
@@ -93,9 +165,10 @@ export default function Signup() {
             이름<span className="text-red-500">*</span>
           </label>
           <input
-            type="password"
+            type="text"
             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
             placeholder="이름(성별)을 입력해 주세요."
+            {...register('name', { required: true })}
           />
         </div>
         {/* 닉네임 */}
@@ -105,13 +178,15 @@ export default function Signup() {
           </label>
           <div className="flex space-x-3">
             <input
-              type="email"
+              type="text"
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-              placeholder="한/영 10자 이내,특수문자,공백 불가"
+              placeholder="한/영 10자 이내, 특수문자, 공백 불가"
+              {...register('nickname', { required: true })}
             />
             <button
               type="button"
               className="rounded-lg bg-interactive-light hover:bg-primary-lightest py-3 px-4 whitespace-nowrap"
+              onClick={handleCheckNickname}
             >
               중복 확인
             </button>
@@ -124,7 +199,8 @@ export default function Signup() {
               type="checkbox"
               className="mr-2 h-4 w-4 border border-gray-300 rounded"
               checked={isChecked}
-              onChange={handleCheckboxChange}
+              onClick={handleCheckboxChange}
+              {...register('terms', { required: true })}
             />
             위치 정보 이용약관(필수)
           </label>
@@ -147,6 +223,7 @@ export default function Signup() {
         <button
           type="submit"
           className="font-bold w-full bg-interactive-light hover:bg-primary-lightest hover:text-white rounded-lg py-3 px-4 whitespace-nowrap"
+          onClick={() => handleSubmit(onSubmit)()}
         >
           가입하기
         </button>
