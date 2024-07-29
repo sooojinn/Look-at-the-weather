513f809e228aea9b154fa07fe0d0e44e87ffeb5e
soojinpark99 <tnwls7263@gmail.com>
2024-07-30 01:53:24 +0900

    refactor: compass 아이콘 컴포넌트화

--- src/components/home/WeatherInfo.tsx
+++ src/components/home/WeatherInfo.tsx
@@ -1,8 +1,8 @@
-import compassIcon from '../../assets/compass_icon.svg';
+import CompassIcon from '../icons/CompassIcon';
 import useWeatherData from '../../hooks/useWeatherData';
 
 export default function WeatherInfoComponent() {
-  const { location, currentTemp, minTemp, maxTemp, isLoading } = useWeatherData();
+  const { location, currentTemp, minTemp, maxTemp, weatherType, isLoading } = useWeatherData();
 
   return (
     <div className="w-full bg-blue-200 h-[292px]">
@@ -11,15 +11,17 @@ export default function WeatherInfoComponent() {
           날씨 정보를 가져오는 중입니다...
         </div>
       ) : (
-        <div className="w-full h-full px-5 text-white ">
-          <div className="mt-[55px] flex gap-2">
-            <img src={compassIcon} alt="Compass" />
-            <span>{location}</span>
-          </div>
-          <div className="mt-2 font-spoqa-han-sans font-bold text-[100px] leading-none">{currentTemp}°</div>
-          <div className="font-bold">조금 쌀쌀해요!</div>
-          <div className="text-sm">
-            최고 {maxTemp}° / 최저 {minTemp}°
+        <div className="w-full h-full px-5 text-white flex">
+          <div>
+            <div className="mt-[55px] flex items-center gap-2">
+              <CompassIcon fill={'white'} />
+              <span>{location}</span>
+            </div>
+            <div className="mt-2 font-spoqa-han-sans font-bold text-[100px] leading-none">{currentTemp}°</div>
+            <div className="font-bold">조금 쌀쌀해요!</div>
+            <div className="text-sm">
+              최고 {maxTemp}° / 최저 {minTemp}°
+            </div>
           </div>
         </div>
       )}
