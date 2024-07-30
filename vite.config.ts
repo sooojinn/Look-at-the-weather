7af1ea324f0a1b2bc0be6ec1e991cbeec43861bf
plumpsqrl9744 <rnwkddn1234@nate.com>
2024-07-31 03:49:00 +0900

    chore : craco alias 적용 완료

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      { find: '@', replacement: '/src' },
-      { find: '@components', replacement: '/src/components' },
-      { find: '@pages', replacement: '/src/pages' },
-      { find: '@common', replacement: '/src/common' },
-    ],
+    alias: {
+      '@': '/src',
+      '@components': '/src/components',
+      '@pages': '/src/pages',
+      '@common': '/src/common',
+    },
   },
 });
