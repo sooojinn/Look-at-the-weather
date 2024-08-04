d8289bd2aa9328f1124276f81d662a72ba320f0d
plumpsqrl9744 <rnwkddn1234@nate.com>
2024-08-04 22:24:37 +0900

    chore: 컴포넌트 브랜치 추가

---
+++ src/config/constants.ts
@@ -0,0 +1,5 @@
+export const BASEURL = import.meta.env.VITE_BASE_URL;
+
+export const SERVICE_KEY = import.meta.env.VITE_SERVICE_KEY;
+
+export const WEATHER_API_URL = import.meta.env.VITE_WEATHER_API_URL;
