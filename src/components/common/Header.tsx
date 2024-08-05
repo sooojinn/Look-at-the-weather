c444376bae2683e250ecbb694d03687ed68daf4c
soojinpark99 <tnwls7263@gmail.com>
2024-08-05 20:46:56 +0900

    style: 헤더에 close 버튼 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,11 +1,14 @@
+import BackBtn from '@components/icons/BackBtn';
+import CloseBtn from '@components/icons/CloseBtn';
 import { ReactNode } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 interface HeaderProps {
-  children: ReactNode;
+  children?: ReactNode;
+  isModal?: boolean;
 }
 
-export default function Header({ children }: HeaderProps) {
+export default function Header({ children, isModal = false }: HeaderProps) {
   const location = useLocation();
   const mainPageList = ['/', '/post', '/postwrite', '/mypage'];
   const isMainPage = mainPageList.includes(location.pathname);
@@ -17,13 +20,11 @@ export default function Header({ children }: HeaderProps) {
       <div className="w-6 h-6">
         {/* 메인 페이지가 아니면 뒤로가기 버튼 생성 */}
         <button className={`${isMainPage ? 'hidden' : 'block'}`} onClick={handleBackBtn}>
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M7.825 13L13.425 18.6L12 20L4 12L12 4L13.425 5.4L7.825 11H20V13H7.825Z" fill="#171719" />
-          </svg>
+          <BackBtn />
         </button>
       </div>
       <div className="font-bold">{children}</div>
-      <div className="w-6 h-6"></div>
+      <div className="w-6 h-6">{isModal && <CloseBtn />}</div>
     </header>
   );
 }
--- src/pages/PostWrite.tsx
+++ src/pages/PostWrite.tsx
@@ -1,8 +1,10 @@
 import FooterNavi from '@/components/common/FooterNavi';
+import Header from '@components/common/Header';
 
 export default function () {
   return (
     <div>
+      <Header isModal={true}>게시글 작성하기</Header>
       게시글 작성 페이지
       <FooterNavi />
     </div>
