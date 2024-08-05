f18247a4c4be236058b2fa03e756412b5a651a02
soojinpark99 <tnwls7263@gmail.com>
2024-08-05 17:50:10 +0900

    refactor: Tag 컴포넌트에서 span 대신 Text 컴포넌트 사용 #41

--- src/components/post/Tags.tsx
+++ src/components/post/Tags.tsx
@@ -1,11 +1,17 @@
+import Text from '@components/common/atom/Text';
+
 interface TagsProps {
   tags: string[];
 }
 
 export default function Tags({ tags }: TagsProps) {
-  return tags.map((tag, index) => (
-    <span key={index} className="inline-block h-auto mr-2 text-[13px] text-gray">
-      #{tag}
-    </span>
-  ));
+  return (
+    <div className="flex flex-wrap gap-x-2 mt-1">
+      {tags.map((tag) => (
+        <Text size="s" color="gray">
+          #{tag}
+        </Text>
+      ))}
+    </div>
+  );
 }
