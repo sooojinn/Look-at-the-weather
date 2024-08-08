1c6435f2aaa0d314fb78db5c9db9d264c5ec91ec
soojinpark99 <tnwls7263@gmail.com>
2024-08-09 01:12:43 +0900

    feat: api url, body 변경에 따른 수정 #38

--- src/components/form/Select.tsx
+++ src/components/form/Select.tsx
@@ -10,14 +10,14 @@ export default function Select({ name, options, maxSelection = 1, control, rules
       rules={rules}
       render={({ field: { onChange, value } }) => (
         <div className="flex flex-wrap gap-2">
-          {options.map(({ key, name }) => {
+          {options.map(({ id, name }) => {
             return (
               <OptionBtn
-                key={key}
+                key={id}
                 name={name}
-                selected={isSelected(value, key)}
+                selected={isSelected(value, id)}
                 onClick={(e) => {
-                  handleOptionClick(e, value, key, maxSelection, onChange);
+                  handleOptionClick(e, value, id, maxSelection, onChange);
                 }}
               />
             );
@@ -28,18 +28,18 @@ export default function Select({ name, options, maxSelection = 1, control, rules
   );
 }
 
-function isSelected(value: number | number[] | null, key: number): boolean {
+function isSelected(value: number | number[] | null, id: number): boolean {
   // value가 배열(다중선택)일 때
   if (Array.isArray(value)) {
-    return value.includes(key);
+    return value.includes(id);
   }
-  return value === key;
+  return value === id;
 }
 
 function handleOptionClick(
   e: React.MouseEvent<HTMLButtonElement>,
   value: number | number[] | null,
-  key: number,
+  id: number,
   maxSelection: number,
   onChange: (value: number | number[] | null) => void,
 ) {
@@ -47,17 +47,17 @@ function handleOptionClick(
 
   // 다중선택이 아닐 때
   if (maxSelection === 1) {
-    onChange(value === key ? null : key);
+    onChange(value === id ? null : id);
     return;
   }
 
   // 다중선택일 때
   if (maxSelection > 1 && Array.isArray(value)) {
-    if (value.includes(key)) {
-      const newValue = value.filter((v) => v !== key);
+    if (value.includes(id)) {
+      const newValue = value.filter((v) => v !== id);
       onChange(newValue);
     } else {
-      const newValue = [...value, key];
+      const newValue = [...value, id];
       if (newValue.length > maxSelection) {
         newValue.shift();
       }
--- src/config/types.ts
+++ src/config/types.ts
@@ -9,13 +9,18 @@ export interface WeatherInfo {
   [key: string]: any;
 }
 
+export interface Location {
+  city: string;
+  district: string;
+}
+
 export interface PostMeta {
   postId: number;
   thumbnail: string;
-  location: string;
+  location: Location;
   SeasonTag: string;
-  WeatherTag: string[];
-  TempTag: string[];
+  WeatherTags: string[];
+  TemperatureTags: string[];
   likeByUser: boolean;
 }
 
@@ -23,27 +28,24 @@ export type TextSize = 'xs' | 's' | 'm' | 'l' | 'xl' | '2xl' | '3xl';
 export type TextColor = 'black' | 'lightBlack' | 'darkGray' | 'gray' | 'lightGray' | 'white' | 'main' | 'disabled';
 export type TextWeight = 'regular' | 'bold';
 
-export interface Location {
-  city: string;
-  district: string;
-}
-
 export interface PostFormData {
   title: string;
   content: string;
-  location: Location;
+  location: Location | null;
   weatherTagIds: number[];
   temperatureTagIds: number[];
   seasonTagId: number | null;
 }
 
-export interface Option {
-  key: number;
+export interface Tag {
+  id: number;
+  category: string;
+  value: string;
   name: string;
 }
 export interface SelectProps {
   name: string;
-  options: Option[];
+  options: Tag[];
   maxSelection?: number;
   control: Control<any>;
   rules?: RegisterOptions; // 유효성 검사 규칙
