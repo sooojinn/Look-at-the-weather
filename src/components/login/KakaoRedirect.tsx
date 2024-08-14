e6d863c2f3869c3f8c129bb30d3f0c99308900e9
soojinpark99 <tnwls7263@gmail.com>
2024-08-14 22:35:56 +0900

    feat: 카카오 로그인 기능 구현 #49

--- src/components/login/KakaoRedirect.tsx
+++ src/components/login/KakaoRedirect.tsx
@@ -1,26 +1,55 @@
 import { BASEURL } from '@/config/constants';
 import axios from 'axios';
 import { useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
+import Text from '@components/common/atom/Text';
 
-export default function KakaoRedirect() {
-  const code = new URL(window.location.href).searchParams.get('code');
-
-  useEffect(() => {
-    const postCode = async () => {
-      // api 확실하게 물어보기
-      const response = await axios.post(
-        `${BASEURL}/api/v1/oauth/kakao?code=${code}`,
-        {},
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        },
-      );
-      return response.data;
-    };
-    postCode();
+const getUserInfo = async (code: string | null) => {
+  const response = await axios.get(`${BASEURL}/api/v1/oauth/kakao?code=${code}`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
   });
 
-  return <div>로그인 중입니다...</div>;
+  return response.data;
+};
+
+export default function KakaoRedirect() {
+  const navigate = useNavigate();
+  const code = new URL(window.location.href).searchParams.get('code');
+
+  const { data, isSuccess, error, isLoading } = useQuery({
+    queryKey: ['userInfo', code],
+    queryFn: () => getUserInfo(code),
+    enabled: !!code,
+  });
+
+  useEffect(() => {
+    if (isSuccess) {
+      console.log(data);
+
+      const { accessToken, refreshToken } = data;
+      localStorage.setItem('accessToken', accessToken);
+      Cookies.set('refreshToken', refreshToken);
+
+      navigate('/');
+    }
+
+    if (error) {
+      console.error('로그인에 실패했습니다:', error);
+      navigate('/');
+    }
+  }, [isSuccess, error]);
+
+  return (
+    <>
+      {isLoading && (
+        <Text color="black" weight="bold">
+          잠시만 기다려 주세요..
+        </Text>
+      )}
+    </>
+  );
 }
