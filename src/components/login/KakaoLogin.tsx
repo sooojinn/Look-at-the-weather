2ef054afaa2053a3b989e624ba15def42db7b495
soojinpark99 <tnwls7263@gmail.com>
2024-08-14 23:40:52 +0900

    style: 카카오 로그인 버튼 퍼블리싱 #49

--- src/components/login/KakaoLogin.tsx
+++ src/components/login/KakaoLogin.tsx
@@ -1,4 +1,5 @@
 import { KAKAO_REST_API_KEY, REDIRECT_URI } from '@/config/constants';
+import kakaoLoginBtn from '/assets/kakao_symbol.svg';
 
 export default function KakaoLogin() {
   const url = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
@@ -8,8 +9,13 @@ export default function KakaoLogin() {
   };
 
   return (
-    <button type="button" onClick={handleKakaoLogin}>
-      로그인 하기
+    <button
+      type="button"
+      className="w-full h-14 bg-[#FEE500] rounded-xl flex justify-center items-center gap-3"
+      onClick={handleKakaoLogin}
+    >
+      <img src={kakaoLoginBtn} width={25} alt="카카오" />
+      <span className="text-l font-bold text-black/85">카카오 로그인</span>
     </button>
   );
 }
