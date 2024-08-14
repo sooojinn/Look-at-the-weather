08224dbb5fd3a34d9ec34d3b1bfed283ac62f945
soojinpark99 <tnwls7263@gmail.com>
2024-08-14 23:03:36 +0900

    chore: 필요없는 스타일 삭제

--- src/components/icons/Spinner.tsx
+++ src/components/icons/Spinner.tsx
@@ -1,5 +1,5 @@
 import spinner from '/assets/loading_spinner.gif';
 
 export default function Spinner({ width = 25 }) {
-  return <img src={spinner} width={width} className="absolute" alt="Loading..." />;
+  return <img src={spinner} width={width} alt="Loading..." />;
 }
