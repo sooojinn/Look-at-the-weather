610543863b36cb41f6e7f45e5eb0ac2240901e0b
soojinpark99 <tnwls7263@gmail.com>
2024-08-25 14:17:14 +0900

    feat: input 입력값 삭제 버튼 기능 추가 #54

--- src/components/form/InputWithLabel.tsx
+++ src/components/form/InputWithLabel.tsx
@@ -4,6 +4,7 @@ import ExclamationMarkIcon from '@components/icons/ExclamationMarkIcon';
 import { ReactNode, useState } from 'react';
 import PasswordToggleBtn from '@components/icons/PasswordToggleBtn';
 import ErrorMessage from './ErrorMessage';
+import InputDeleteBtn from '@components/icons/InputDeleteBtn';
 
 interface InputWithLabelProps {
   name: string;
@@ -29,10 +30,39 @@ export default function InputWithLabel({
   button,
 }: InputWithLabelProps) {
   const [isPasswordVisible, setPasswordVisible] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [showDeleteBtn, setShowDeleteBtn] = useState(false);
 
   const togglePasswordVisibility = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setPasswordVisible(!isPasswordVisible);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInputValue(value);
+    if (value) {
+      setShowDeleteBtn(true);
+    } else {
+      setShowDeleteBtn(false);
+    }
+  };
+
+  const handleDeleteClick = () => {
+    setInputValue('');
+    setShowDeleteBtn(false);
+  };
+
+  const handleFocus = () => {
+    if (inputValue) {
+      setShowDeleteBtn(true);
+    }
+  };
+
+  const handleBlur = () => {
+    setTimeout(() => {
+      setShowDeleteBtn(false); // 포커스 아웃 시 삭제 버튼 숨김 (클릭 이벤트가 먼저 처리되도록 setTimeout 사용)
+    }, 0);
   };
 
   const inputType = type === 'password' && isPasswordVisible ? 'text' : type;
@@ -51,16 +81,23 @@ export default function InputWithLabel({
               isDisabled ? '!text-lightGray !bg-white' : ''
             }`}
             placeholder={placeholder}
-            {...register(name, rules)}
+            {...register(name, {
+              ...rules,
+              onChange: handleInputChange,
+            })}
+            value={inputValue}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
           />
           <div className="absolute right-4 bottom-1/2 transform translate-y-1/2 flex items-center h-full">
             {hasError && <ExclamationMarkIcon width={20} fill="#ff4242" />}
             {!hasError && type === 'password' && (
               <PasswordToggleBtn onToggle={togglePasswordVisibility} isVisible={isPasswordVisible} />
             )}
+            {!hasError && type !== 'password' && showDeleteBtn && <InputDeleteBtn onClick={handleDeleteClick} />}
           </div>
         </div>
-        <div className="ml-3">{button}</div>
+        {button && <div className="ml-3">{button}</div>}
       </div>
       {hasError && <ErrorMessage errors={errors} name={name} />}
     </div>
