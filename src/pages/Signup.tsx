a6048ddec9dfa14e6c71931071ef23b4ba7042fd
soojinpark99 <tnwls7263@gmail.com>
2024-08-27 01:51:30 +0900

    feat: 회원가입 폼 이름 유효성 검사 규칙 추가 #54

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -193,6 +193,10 @@ export default function Signup() {
             register={register}
             rules={{
               required: '이름을 입력해 주세요.',
+              pattern: {
+                value: /^[a-zA-Z가-힣]{1,10}$/,
+                message: '한/영 10자 이내(특수문자, 공백 불가)',
+              },
             }}
             errors={errors}
             setValue={setValue}
