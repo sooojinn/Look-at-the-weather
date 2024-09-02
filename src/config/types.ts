5319263dc343b623227870d764df04beac26cd5b
soojinpark99 <tnwls7263@gmail.com>
2024-09-02 21:28:22 +0900

    fix: merge 후 충돌 해결 #64

--- src/config/types.ts
+++ src/config/types.ts
@@ -37,7 +37,17 @@ export interface PostMeta {
 }
 
 export type TextSize = 'xs' | 's' | 'm' | 'l' | 'xl' | '2xl' | '3xl';
-export type TextColor = 'black' | 'lightBlack' | 'darkGray' | 'gray' | 'lightGray' | 'white' | 'blue';
+export type TextColor =
+  | 'black'
+  | 'lightBlack'
+  | 'darkGray'
+  | 'gray'
+  | 'lightGray'
+  | 'white'
+  | 'main'
+  | 'disabled'
+  | 'error'
+  | 'success';
 export type TextWeight = 'regular' | 'bold';
 export type TextMargin = string;
 
@@ -66,9 +76,64 @@ export type PostFilterModalProps = {
 
 export type SectionKey = 'location' | 'weather' | 'temperature' | 'season';
 
+export interface DistrictProps {
+  city_id: number;
+  district_id: number;
+  district: string;
+}
+
+export type DistrictArray = DistrictProps[];
+
+export interface PostFormData {
+  title: string;
+  content: string;
+  location: Location;
+  weatherTagIds: number[];
+  temperatureTagIds: number[];
+  seasonTagId: number | null;
+  imageId: number[];
+}
+
+export interface Tag {
+  id: number;
+  category: string;
+  value: string;
+  name: string;
+}
+export interface SelectProps {
+  name: keyof PostFormData;
+  options: Tag[];
+  maxSelection?: number;
+  control: Control<any>;
+  rules?: RegisterOptions; // 유효성 검사 규칙
+}
+
+export interface FileProps {
+  name: keyof PostFormData;
+  rules?: RegisterOptions<PostFormData, keyof PostFormData>;
+  setValue: UseFormSetValue<PostFormData>;
+  register: UseFormRegister<PostFormData>;
+}
+
+export interface ErrorResponse {
+  errorCode?: string;
+  errorMessage?: string;
+}
+
 export interface VerifyCodeProps {
   email: string;
   code: string;
+}
+
+export interface FormMethods<T extends FieldValues> {
+  register: UseFormRegister<T>;
+  setValue: UseFormSetValue<T>;
+  setError: UseFormSetError<T>;
+  clearErrors: UseFormClearErrors<T>;
+  trigger: UseFormTrigger<T>;
+  getValues: UseFormGetValues<T>;
+  watch: UseFormWatch<T>;
+  formState: { errors: FieldErrors<T> };
 }
 
 export interface SignupForm {
@@ -81,4 +146,10 @@ export interface SignupForm {
   terms: boolean;
 }
 
-export type DistrictArray = DistrictProps[];
+export interface RegisterForm {
+  email: string;
+  password: string;
+  name: string;
+  nickname: string;
+  isSocial: boolean;
+}
