5a0c8e257cf273c57141cd595fc10f428d9dd50a
soojinpark99 <tnwls7263@gmail.com>
2024-09-09 18:14:22 +0900

    refactor: 자잘한 코드 수정

--- src/components/common/molecules/Button.tsx
+++ src/components/common/molecules/Button.tsx
@@ -65,7 +65,6 @@ export default function Button({
 
   return (
     <button
-      type="button"
       onClick={onClick}
       disabled={disabled || isSubmitting}
       style={{ width: width ? `${width}px` : '100%' }}
