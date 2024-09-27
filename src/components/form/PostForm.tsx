690aeb389c341bd19924f7b38eab75640714af39
soojinpark99 <tnwls7263@gmail.com>
2024-09-27 18:24:23 +0900

    fix: 이미지 삭제 관련 버그 해결

--- src/components/form/PostForm.tsx
+++ src/components/form/PostForm.tsx
@@ -106,6 +106,7 @@ export default function PostForm({ type, defaultValues, onSubmit }: PostWriteFor
               label="오늘의 룩을 올려주세요"
               description="사진 추가는 최대 3장까지 가능합니다."
               rules={{ required: true }}
+              defaultImageIds={defaultValues.imageIds}
             />
             <div className="flex flex-col gap-4">
               <Text size="l" weight="bold">
