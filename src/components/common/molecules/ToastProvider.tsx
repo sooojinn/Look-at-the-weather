3b852bfb93a665e263b1737a3c751e36598f619c
soojinpark99 <tnwls7263@gmail.com>
2024-10-17 22:32:48 +0900

    chore: 도메인 변경에 따른 카카오 redirect uri 변경

--- src/components/common/molecules/ToastProvider.tsx
+++ src/components/common/molecules/ToastProvider.tsx
@@ -14,6 +14,7 @@ export function ToastProvider() {
       hideProgressBar
       closeButton={false}
       newestOnTop={false}
+      pauseOnHover={false}
       pauseOnFocusLoss={false}
       transition={CustomTransition} // 사용자 정의 transition 적용
       position="bottom-center"
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -6,7 +6,7 @@ export const WEATHER_API_URL = import.meta.env.VITE_WEATHER_API_URL;
 
 export const KAKAO_REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
 
-export const REDIRECT_URI = 'https://look-at-the-weather.vercel.app/oauth';
+export const REDIRECT_URI = 'https://www.lookattheweather.store/oauth';
 
 export const TAGS = [
   { id: 1, category: 'weather', value: 'CLOUDY', name: '흐림' },
