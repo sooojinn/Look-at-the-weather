f211d653598b1a7ee20ce519b69b0713d9053c39
soojinpark99 <tnwls7263@gmail.com>
2024-11-19 23:51:36 +0900

    feat: 게시글 이미지 로딩 전 placeholder 설정

--- src/components/post/PostImg.tsx
+++ src/components/post/PostImg.tsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+import placeholderImg from '/assets/placeholder_img.svg';
+
 interface PostListImgProps {
   imgUrl: string;
 }
 
 export default function PostImg({ imgUrl }: PostListImgProps) {
-  return <img src={imgUrl} className="w-full h-auto object-cover aspect-[3/4]" alt="thumbnail" />;
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  return (
+    <img
+      src={isLoaded ? imgUrl : placeholderImg}
+      onLoad={() => setIsLoaded(true)}
+      onError={() => setIsLoaded(true)}
+      className="w-full h-auto object-cover aspect-[3/4]"
+      alt="thumbnail"
+    />
+  );
 }
