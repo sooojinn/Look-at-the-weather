52942108b97b1ffe5a8e6627fcf9dfd0a7ebbd1e
soojinpark99 <tnwls7263@gmail.com>
2025-01-16 22:11:59 +0900

    chore: BrowserRouter 임시 코드 삭제

--- src/lib/ReactQueryProvider.tsx
+++ src/lib/ReactQueryProvider.tsx
@@ -2,15 +2,13 @@
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
-import { BrowserRouter } from 'react-router-dom';
 
 export default function ReactQueryProvider({ children }: React.PropsWithChildren) {
   const [client] = useState(new QueryClient());
 
   return (
     <QueryClientProvider client={client}>
-      {/* BrowserRouter 임시 설정 */}
-      <BrowserRouter>{children}</BrowserRouter>
+      {children}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
