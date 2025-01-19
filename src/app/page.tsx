821b88d7928316a48d05bb74814a2f2f06ae7327
plumpsqrl9744 <rnwkddn1234@nate.com>
2025-01-19 19:35:06 +0900

    refactor: 메인 페이지 모달 위치 변경 #115

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,8 @@ import Logo from '@components/common/atom/Logo';
 import HomeWeatherWidget from '@/components/weather/HomeWeatherWidget';
 import TodayBestWearList from '@/components/post/TodayBestWearList';
 import GuidePanel from '@components/common/organism/GuidePanel';
-import TempGuideModal from '@components/common/molecules/TempGuideModal';
-import ManualGuide from '@components/common/molecules/ManualGuide';
-import { useGuideManageStore } from '@/store/guideManageStore';
 
 export default function Home() {
-  const { isLookGuideModalOpen, isManualGuideModalOpen } = useGuideManageStore();
   return (
     <div className="max-w-md min-h-screen flex flex-col items-center justify-start relative">
       <Header>
@@ -20,8 +16,6 @@ export default function Home() {
       <GuidePanel />
       <TodayBestWearList />
       <FooterNavi />
-      {isLookGuideModalOpen && <TempGuideModal />}
-      {isManualGuideModalOpen && <ManualGuide />}
     </div>
   );
 }
