da9c8208a3b573c63beccb9a760d688636b20ca2
soojinpark99 <tnwls7263@gmail.com>
2025-02-08 23:10:48 +0900

    feat: 새로고침 후 api 요청 전 토큰 재발급 로직 추가 #118

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -15,11 +15,6 @@ export const instance = axios.create({
   timeout: 10000,
 });
 
-// export const restoreAuthInstance = axios.create({
-//   baseURL: BASEURL,
-//   timeout: 10000,
-// });
-
 export const reissueInstance = axios.create({
   baseURL: BASEURL,
   timeout: 10000,
@@ -29,27 +24,32 @@ export const setAccessToken = (token: null | string) => {
   accessToken = token;
   if (token) {
     instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    // restoreAuthInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   } else {
     delete instance.defaults.headers.common['Authorization'];
-    // delete restoreAuthInstance.defaults.headers.common['Authorization'];
   }
-
-  console.log('instance Authorization:', instance.defaults.headers.common['Authorization']);
-  // console.log('reissueInstance Authorization:', restoreAuthInstance.defaults.headers.common['Authorization']);
 };
-
-// const getAccessToken = () => {
-//   return accessToken;
-// };
 
 const REISSUE_REQUIRED_ERROR_CODES = ['ACCESS_TOKEN_EXPIRED', 'INVALID_CREDENTIALS'];
 const SESSION_EXPIRED_ERRORS = ['REFRESH_TOKEN_EXPIRED', 'NOT_FOUND_COOKIE'];
 
 instance.interceptors.response.use(
-  (response) => {
-    return response;
+  async (config) => {
+    const authState = sessionStorage.getItem('auth-storage');
+    const isLogin = authState ? JSON.parse(authState).state?.isLogin : undefined;
+    const isToken = !!accessToken;
+
+    if (isLogin && !isToken) {
+      const response = await reissue();
+      const { accessToken } = response;
+      if (accessToken) {
+        setAccessToken(accessToken);
+        config.headers.Authorization = `Bearer ${accessToken}`;
+        return config;
+      }
+    }
+    return config;
   },
+
   async (error) => {
     const { errorCode } = error.response.data;
 
@@ -70,31 +70,7 @@ instance.interceptors.response.use(
   },
 );
 
-// // 로그인한 상태에서 새로고침 후 토큰이 초기화됐을 경우 토큰을 먼저 재발급 받고 요청
-// restoreAuthInstance.interceptors.request.use(
-//   async (config) => {
-//     const authState = sessionStorage.getItem('auth-storage');
-//     const isLogin = authState ? JSON.parse(authState).state?.isLogin : undefined;
-//     const isToken = !!getAccessToken();
-
-//     console.log('현재 저장된 token', getAccessToken());
-//     console.log('요청 헤더', config.headers);
-
-//     if (isLogin && !isToken) {
-//       const response = await reissue();
-//       const { accessToken } = response;
-//       if (accessToken) {
-//         setAccessToken(accessToken);
-//         config.headers.Authorization = accessToken;
-//         return config;
-//       }
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error),
-// );
-
-// 리이슈 요청 처리
+// 리이슈 요청 에러 처리
 reissueInstance.interceptors.response.use(
   (response) => {
     return response;
