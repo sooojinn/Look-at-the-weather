4e0c020a6a599190ffe7e664f2ba2326711b76f0
soojinpark99 <tnwls7263@gmail.com>
2025-03-05 18:57:03 +0900

    refactor: post 폴더 내부 컴포넌트 리팩토링 진행중2 #119

--- src/components/post/atom/Heart.tsx
+++ src/components/post/atom/Heart.tsx
@@ -1,15 +1,8 @@
-import { AxiosError } from 'axios';
-import { useEffect, useState } from 'react';
 import Text from '../../common/atom/Text';
-import { deleteLike, postLike } from '@/api/apis';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { showToast } from '../../provider/ToastProvider';
-import { ErrorResponse } from '@/config/types';
 import RedHeartIcon from '@components/icons/hearts/RedHeartIcon';
 import EmptyHeartIcon from '@components/icons/hearts/EmptyHeartIcon';
-import { useAuthStore } from '@/store/authStore';
-import { useRouter } from 'next/navigation';
 import LoginPromptModal from '@/components/modal/LoginPromptModal';
+import { useHeart } from '@/hooks/useHeart';
 
 interface HeartProps {
   fill?: string;
@@ -26,71 +19,16 @@ export default function Heart({
   hasUserNumber,
   likedCount: initialLikedCount,
 }: HeartProps) {
-  const [isLiked, setIsLiked] = useState<boolean>(liked);
-  const [likedCount, setLikedCount] = useState(initialLikedCount);
-  const [showLoginPromptModal, setShowLoginPromptModal] = useState(false);
-
-  const isLogin = useAuthStore((state) => state.isLogin);
-  const queryClient = useQueryClient();
-  const router = useRouter();
-
-  useEffect(() => {
-    setIsLiked(liked);
-    setLikedCount(initialLikedCount);
-  }, [liked, initialLikedCount]);
-
-  const toggleLikeMutation = useMutation({
-    mutationFn: async () => {
-      return isLiked ? await deleteLike(postId) : await postLike(postId);
-    },
-    onSuccess: ({ likedCount: newLikedCount }) => {
-      setIsLiked((prev) => !prev);
-      setLikedCount(newLikedCount);
-
-      // 좋아요 여부 동기화를 위해 post 쿼리 무효화
-      queryClient.invalidateQueries({ queryKey: ['post', 'list'], refetchType: 'none' });
-      queryClient.invalidateQueries({ queryKey: ['post', 'detail', postId], refetchType: 'none' });
-    },
-    onError: (error: AxiosError<ErrorResponse>) => {
-      if (error.response) {
-        const { status, data } = error.response;
-        if (status === 400) {
-          showToast(`${data.errorMessage}`);
-        } else if (status !== 401) {
-          showToast('에러가 발생했습니다. 다시 시도해주세요.');
-          console.error(`${status} 에러: ${error}`);
-        }
-      } else {
-        console.error('예상치 못한 에러가 발생했습니다.', error);
-      }
-    },
-  });
-
-  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault();
-
-    if (isLogin) toggleLikeMutation.mutate();
-    else setShowLoginPromptModal(true);
-  };
+  const { isLiked, likedCount, showLoginPromptModal, handleLikeClick, handleCloseLoginPrompt, handleContinueLogin } =
+    useHeart({ liked, postId, initialLikedCount });
 
   return (
     <>
-      <div onClick={handleClick} className="flex items-center gap-x-2">
+      <div onClick={handleLikeClick} className="flex items-center gap-x-2">
         {isLiked ? <RedHeartIcon /> : <EmptyHeartIcon fill={fill} />}
         {hasUserNumber && <Text>{likedCount || 0}</Text>}
       </div>
-      {showLoginPromptModal && (
-        <LoginPromptModal
-          onCancel={(e) => {
-            e.stopPropagation();
-            setShowLoginPromptModal(false);
-          }}
-          onContinue={(e) => {
-            e.stopPropagation();
-            router.push('/login');
-          }}
-        />
-      )}
+      {showLoginPromptModal && <LoginPromptModal onCancel={handleCloseLoginPrompt} onContinue={handleContinueLogin} />}
     </>
   );
 }
