09be407f5cb7556b1a28f7e4dc2f1a78f252eb64
soojinpark99 <tnwls7263@gmail.com>
2025-04-29 10:58:55 +0900

    feat: 내가 좋아요한 게시물 페이지 ssr 적용 #125

--- src/app/mypage/like/page.tsx
+++ src/app/mypage/like/page.tsx
@@ -1,23 +1,24 @@
-'use client';
-
-import { getMyLikedPosts } from '@/api/apis';
-import VirtualInfiniteScroll from '@/components/post/organism/VirtualInfiniteScroll';
 import FooterNavi from '@/components/common/organism/FooterNavi';
 import Header from '@/components/common/organism/Header';
-import MyLikedPostEmpty from '@/components/placeholder/MyLikedPostEmpty';
+import MyLikedPosts from '@/components/post/page/MyLikedPosts';
+import InitQuery from '@/components/provider/InitQuery';
+import { BASEURL } from '@/config/constants';
+import { fetchWithAuth } from '@/lib/fetcher';
 
-export default function MyLikedPost() {
+export default async function MyLikedPostsPage() {
+  const myLikedPostsData = await fetchWithAuth(`${BASEURL}/likes/posts?page=0&size=10`);
+
   return (
     <>
       <Header>내가 좋아요한 게시물</Header>
-      <div className="flex-grow">
-        <VirtualInfiniteScroll
-          queryKey={['post', 'list', 'myLikedPosts']}
-          queryFn={getMyLikedPosts}
-          headerText="내가 좋아요한 게시물"
-          placeholderComp={MyLikedPostEmpty}
-        />
-      </div>
+      <InitQuery
+        queryKey={['post', 'list', 'myLikedPosts']}
+        data={{
+          pages: [myLikedPostsData],
+          pageParams: [0],
+        }}
+      />
+      <MyLikedPosts />
       <FooterNavi />
     </>
   );
